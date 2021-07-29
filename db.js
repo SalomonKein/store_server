a559846adf2def501cb047e2b6a86d9150f91272
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2021-07-29 17:12:16 +0300

    without url

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ module.exports = new Sequelize(
 process.env.DB_NAME,
 process.env.DB_USER,
 process.env.DB_PASSWORD,
-process.env.DB_URL,
 {
     dialect: 'postgres',
     host: process.env.DB_HOST,
