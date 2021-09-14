1e6a23158d63ff807e8abad2b8a8157495f4d71c
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2021-09-14 15:52:46 +0300

    CORS

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors())
+app.options('*', cors());
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static',)))
 app.use(fileUpload({}))
